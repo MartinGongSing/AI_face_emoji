8c89667792b93cd50ea0f8a8b4bf851b5a648349
MartinGongSing <47624744+MartinGongSing@users.noreply.github.com>
2021-11-17 15:27:23 +0100

    test1 side

--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
 import cv2
 
-# Load the cascade
+# Load the cascade ---- LBP trained
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 #https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
@@ -10,9 +10,13 @@ cap = cv2.VideoCapture(0)
 # To use a video file as input 
 # cap = cv2.VideoCapture('filename.mp4')
 
-emoji = cv2.imread("drop.jpg") #emoji
+drop = cv2.imread("drop.jpg") #drop
+wink = cv2.imread("wink.png") #wink
+right = cv2.imread("right.png") #right
+left = cv2.imread("left.png") #right
 
 
+previous_ey = 0 ## trying to change emoji based on eye hight
 
 while True:
     # Read the frame
@@ -22,16 +26,15 @@ while True:
     # Detect the faces
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
+    
+
     ####### Draw on each face #######
 
     for (x, y, w, h) in faces:
 
-        w1 = w/2
-        h1 = h/2
-        # size emoji 
+       
 
-        dim = (w,h)
-        resized = cv2.resize(emoji, dim)
+        
 
 
         # show face in square
@@ -44,28 +47,68 @@ while True:
         roi_color = img[y:y+h, x:x+w]
        
         eyes = eye_cascade.detectMultiScale(roi_gray)
+        emoji = drop
+
+
+        
+       
+
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
-        ####### put emoji on face #######
+            eye_c = ey + eh/2
 
-        print(img[x:x+w, y:y+h,:].shape)
-        print(resized[0:w,0:h,:].shape)
+
+            # trying to change emoji based on eye hight
+            if (previous_ey - 0.5) <= eye_c <= (previous_ey + 0.5) : 
+                emoji = wink
+                print ("wink")
+
+            # if (previous_ey + 0.5) <= eye_c:
+            #     emoji = right
+
+            # elif eye_c <= (previous_ey - 0.5):
+            #     emoji = left
+
+
+            else :
+                emoji = drop
+                print("drop")
+            
+            print(previous_ey, "\n", eye_c)
+
+
+        previous_ey = eye_c 
+            
+            
+        #################################
+        ####### put emoji on face #######
+        #################################
+        
+        w1 = w/2
+        h1 = h/2
+        # size emoji 
+        dim = (w,h)
+        resized = cv2.resize(emoji, dim)
+
+        # print(img[x:x+w, y:y+h,:].shape)
+        # print(resized[0:w,0:h,:].shape)
 
         if img[x:x+w, y:y+h,:].shape !=  resized[0:w,0:h,:].shape: 
             added_img = img
-            print("Face not detected")
+            # print("Face not detected")
         else : 
-            print(faces)
-            print("Face detected")
+            # print("Face detected")
             added_img = cv2.addWeighted(img[x:x+w, y:y+h,:], 0, resized[0:w,0:h,:], 1, 0)
 
 
         
             
-            img[y:y+h, x:x+w,:] = added_img
+            img[y:y+h, x:x+w,:] = added_img         # Comment this to remove the emoji
 
-
+        #################################
+        #################################
+        #################################
         
 
     # Display
