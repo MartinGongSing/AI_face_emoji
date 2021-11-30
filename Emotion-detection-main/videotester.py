bd3762f6754248956a81e664c69df4038600d20c
MartinGongSing <47624744+MartinGongSing@users.noreply.github.com>
2021-11-30 09:36:34 +0100

    comments

--- Emotion-detection-main/videotester.py
+++ Emotion-detection-main/videotester.py
@@ -10,23 +10,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # load model
-model = load_model("best_model.h5")
+model = load_model("best_model.h5") # --> https://www.youtube.com/watch?v=G1Uhs6NVi-M   8'45
 
-
+# find face 
 face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-
+#open camera
 cap = cv2.VideoCapture(0)
 
 
-
+# load emoji
 angry    = cv2.imread("../emoji/angry.png")
 disgust  = cv2.imread("../emoji/disgust.png")
-fear     = cv2.imread("../emoji/fear.png")
+fear     = cv2.imread("../emoji/fear.png", cv2.IMREAD_UNCHANGED)
 happy    = cv2.imread("../emoji/happy.png")
 sad      = cv2.imread("../emoji/sad.png")
 surprise = cv2.imread("../emoji/surprised.png")
 neutral  = cv2.imread("../emoji/neutral.png")
+mask     = cv2.imread("../emoji/mask.png")
 
 
 
@@ -37,35 +38,51 @@ while True:
         continue
     gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
 
+    # puts face coord in list
     faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
 
     for (x, y, w, h) in faces_detected:
 
-        
-
+        #make rectangle arround face
         cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
+        #make transformations on face
         roi_gray = gray_img[y:y + w, x:x + h]  # cropping region of interest i.e. face area from  image
         roi_gray = cv2.resize(roi_gray, (224, 224))
         img_pixels = image.img_to_array(roi_gray)
         img_pixels = np.expand_dims(img_pixels, axis=0)
         img_pixels /= 255
 
+        # based on model(.h5) predicts in list the emotion catured on face
         predictions = model.predict(img_pixels)
 
-        # find max indexed array
+
+        # find max indexed array -> most suitable
         max_index = np.argmax(predictions[0])
 
+        #list of available emotions
         emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+        #gives the name to the emotion
         predicted_emotion = emotions[max_index]
 
+        #puts text on top of square arround face
         cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-        print(predicted_emotion)
+        # print(predicted_emotion)
 
         #################################
         ####### put emoji on face #######
         #################################
+        
+        #if nothing
         emoji = neutral
+        #if emotion detected
+
+
+        # if emotion2 == 'masked' : 
+        #     emoji = mask
+        # else : 
+
+
         if predicted_emotion == 'angry':
             emoji = angry
         elif predicted_emotion == 'disgust':
@@ -81,15 +98,19 @@ while True:
         elif predicted_emotion == 'surprise':
             emoji = surprise
         else :
-            emoji = neutral
+            emoji = mask
 
-
-        w1 = w/2
-        h1 = h/2
         # size emoji 
+        # w1 = w/2
+        # h1 = h/2
+
         dim = (w,h)
         resized = cv2.resize(emoji, dim)
         
+        #test_img : the one captured by camera
+
+        #if : if no faces are detected or if dim did not work -> do nothing
+        #ellse : if faces are detected -> addWeighted : superpose emoji + camera
 
         if test_img[x:x+w, y:y+h,:].shape !=  resized[0:w,0:h,:].shape: 
             added_img = test_img
@@ -98,9 +119,7 @@ while True:
             # print("Face detected")
             added_img = cv2.addWeighted(test_img[x:x+w, y:y+h,:], 0, resized[0:w,0:h,:], 1, 0)
 
-
-        
-            
+            #right axis + add of images
             test_img[y:y+h, x:x+w,:] = added_img         # Comment this to remove the emoji
 
         #################################
@@ -109,7 +128,7 @@ while True:
 
 
 
-
+    #necessary transformations for display
     resized_img = cv2.resize(test_img, (1000, 700))
     cv2.imshow('Facial emotion analysis ', resized_img)
 
